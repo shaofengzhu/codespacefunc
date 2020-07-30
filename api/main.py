5da43d061b275579c457558185dbc4b4bb8578ca
Shaofeng Zhu <shaozhu@microsoft.com>
2020-07-30 14:37:44 -0700

    Support GET invoke

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import azure.functions as func
+import json
 import logging
 import sys
+from urllib.parse import unquote
 
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).absolute().parent))
@@ -18,11 +20,21 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Try the Azure Functions name if the "natural" name was missing
         import __app__.Functions as Functions
 
+    invokeFunction = False
     if req.method == "POST":
         try:
+            invokeFunction = True
             payload = req.get_json()
         except Exception as ex:
             return func.HttpResponse(f"Invalid request: {ex}", status_code=400)
+
+    _, _, param = req.url.partition("?")
+    if req.method == "GET" and param.startswith("invoke="):
+        invokeFunction = True
+        payload = param.partition("=")[2]
+        payload = json.loads(unquote(payload))
+
+    if invokeFunction:
         try:
             return func.HttpResponse(
                 execute_function(Functions, payload),
